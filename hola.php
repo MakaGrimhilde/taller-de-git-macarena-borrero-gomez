0aba2502cd717cdb133ab01e7a5a56358db93170
MakaGrimhilde <simtoclatro@gmail.com>
2021-05-16 20:05:59 +0200

    Añadido el autor del programa y su email

--- hola.php
+++ hola.php
@@ -1,3 +1,5 @@
 <?php
+// Autor: Macarena Borrero Gómez <mborrerogomez@iessansebastian.com>
+// El nombre por defecto es Mundo
 $nombre = isset($argv[1]) ? $argv[1] : "Mundo";
 @print "Hola, {$nombre}\n";
